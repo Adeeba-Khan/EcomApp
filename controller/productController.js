913796975fef6354ae04ddb99cb37aebdab6f707
Adeeba-Khan <adeebacs0008@gmail.com>
2026-02-26 14:21:13 +0530

    forth  commit

--- controller/productController.js
+++ controller/productController.js
@@ -1,11 +1,15 @@
-
+const path= require(`path`);
 let products = [
     { id: 1, name: "Laptop", price: 50000 },
     { id: 2, name: "Phone", price: 20000 }
 ];
+
+
 const getProducts =(req,res,next)=>{
 
-    res.send("Fetch all products");
+    // res.send("Fetch all products");
+res.sendFile(path.join(__dirname, '..', 'views', 'products.html'));
+
 }
 const postProducts = (req,res,next)=>{
     res.send("Add a new product");
